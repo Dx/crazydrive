52ef1d645521a58c462b954cd0035833a0b9be50
David Cruz <dequis@gmail.com>
2015-09-22 23:50:33 -0500

    Initial Commit

---
+++ CrazyDrive/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CrazyDrive
+//
+//  Created by Dx on 22/09/15.
+//  Copyright © 2015 Palmera. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
